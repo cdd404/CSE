56e003b9a76f2b4af2430b896d2f70a2e6ae08ef
cdd404 <davidovwilliam@gmail.com>
2026-01-02 14:11:22 +0530

    Create Oops8.java

---
+++ Oops8.java
@@ -0,0 +1,30 @@
+package jo;
+
+class Outer {
+    String so = "This is Outer Class";
+
+    void display() {
+        System.out.println(so);
+    }
+
+    void test() {
+        Inner obj2 = new Inner();
+        obj2.display();
+    }
+
+    class Inner {
+        String si = "This is inner Class";
+
+        void display() {
+            System.out.println(si);
+        }
+    }
+}
+
+class InnerClassDemo {
+    public static void main(String args[]) {
+        Outer obj1 = new Outer();
+        obj1.display();
+        obj1.test();
+    }
+}
